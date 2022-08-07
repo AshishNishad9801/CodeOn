b82ea66272a73885ba629eca3420f36a30a974fb
Ashish Nishad <81793588+AshishNishad9801@users.noreply.github.com>
2022-08-07 12:23:20 +0530

    Update Home.js
    Adding git repo in footer and declaring team name who build this project.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="https://github.com/AshishNishad9801/CodeOn/"> Code Tornades </a>
                 </h4>
             </footer>
         </div>
