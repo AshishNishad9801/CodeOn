45d4a583834ccf480f64ba585b2426813af1f60f
Hrid <hridchakraborty@gmail.com>
2023-06-27 15:44:25 +0300

    Migrated Project to Vite for Build optimizations

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
+import express from 'express'
+import http from 'http'
+import path from 'path'
+import { Server } from 'socket.io'
 
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.static('dist'));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const userSocketMap = {};
