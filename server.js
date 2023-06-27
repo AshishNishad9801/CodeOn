f621ee8585256c83027c79b0c505b5bb9760f08b
Hrid <hridchakraborty@gmail.com>
2023-06-27 11:42:05 +0300

    Relocated Firebase Config Details to .env file

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- src/fire.js
+++ src/fire.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyBR1-uObgs0YjxOBZrymeWbbqIsDbVG480",
-    authDomain: "code-on-6634a.firebaseapp.com",
-    projectId: "code-on-6634a",
-    storageBucket: "code-on-6634a.appspot.com",
-    messagingSenderId: "1072326405710",
-    appId: "1:1072326405710:web:aacc0e6e79e4474f41dd1f",
-    measurementId: "G-8MVMYN5KFJ"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
   // Initialize Firebase
